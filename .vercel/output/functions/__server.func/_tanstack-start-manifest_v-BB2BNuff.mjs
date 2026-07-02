f40898e150db3465a34ab66a71c49561d0406cef
Your Name <you@example.com>
2026-07-02 20:18:05 +0530

    try one

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-BB2BNuff.mjs
@@ -0,0 +1,25 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-BB2BNuff.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "C:/xampp/htdocs/khushiii-dream-makers-fixed/khushiii-dream-makers-main/src/routes/__root.tsx",
+		children: ["/", "/message"],
+		preloads: ["/assets/index-C0CAUKeF.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-C0CAUKeF.js"
+		} }]
+	},
+	"/": {
+		filePath: "C:/xampp/htdocs/khushiii-dream-makers-fixed/khushiii-dream-makers-main/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-B53l76el.js"]
+	},
+	"/message": {
+		filePath: "C:/xampp/htdocs/khushiii-dream-makers-fixed/khushiii-dream-makers-main/src/routes/message.tsx",
+		children: void 0,
+		preloads: ["/assets/message-Bj3k4XCj.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
